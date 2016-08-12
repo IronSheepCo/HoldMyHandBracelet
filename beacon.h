2faa914bc5cf75a5a68e0f5bb45b59399afaec17
Radu Dita <hades2510@yahoo.com>
2016-08-12 12:46:47 +0300

    is_area is used to indicate if a hotspot beacon has a far area associated with it

--- beacon.h
+++ beacon.h
@@ -6,8 +6,9 @@
 typedef struct{
     int16_t peer_address;
     float   coef;
-    //true if this should be use as an area indicator instead of a 
+    //first byte is true if this should be use as an area indicator instead of a 
     //beacon for getting pin point location
+    //second byte is true if the beacon has a far hot spot, 0 otherwise
     uint8_t is_area;
 }peer_coef;
 
