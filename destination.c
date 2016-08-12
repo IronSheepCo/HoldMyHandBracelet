a633cdbf504f0d31e35574c3e8da3585e6043279
Radu Dita <hades2510@yahoo.com>
2016-08-12 13:24:40 +0300

    added destination files, used for knowing where to take the user

---
+++ destination.c
@@ -0,0 +1,3 @@
+#include "destination.h"
+
+uint8_t final_destination_point = 4;
---
+++ destination.h
@@ -0,0 +1,8 @@
+#ifndef __HOLDMYHAND_DESTINATION_H__
+#define __HOLDMYHADN_DESTINATION_H__
+
+#include <stdint.h>
+
+extern uint8_t final_destination_point;
+
+#endif
